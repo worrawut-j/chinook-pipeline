60f0b8546b7eadfc8683b830e91fccdc7ece5ce9
worrawut-j <152259934+worrawut-j@users.noreply.github.com>
2024-10-30 14:24:30 +0700

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,8 +1,7 @@
-# Databricks notebook source
 import pandas as pd
-import configparser
+import math
 import os
-from datetime import datetime
+import configparser
 
 # Get the parent path of the notebook
 notebook_path = dbutils.entry_point.getDbutils().notebook().getContext().notebookPath().get()
@@ -14,31 +13,12 @@ config = configparser.ConfigParser()
 config.read('./pipeline.conf')
 inputPath = config.get('DEFAULT', 'INPUT_PATH')
 outputPath = config.get('DEFAULT', 'OUTPUT_PATH')
-testResultPath = config.get('DEFAULT', 'TEST_RESULT_PATH')
 
-# read files
-tracksInput = pd.read_csv(inputPath)
-tracksOutput = pd.read_csv(outputPath)
+# Extract
+tracks = pd.read_csv(inputPath)
 
-# open test result file
-f = open(testResultPath, "a")
-
-# write datetime
-f.write(datetime.now().strftime("%d-%m-%Y %H:%M:%S") + '\n')
-
-# Case 1
-unitPriceType = tracksOutput.dtypes['UnitPrice']
-if unitPriceType == 'int64':
-    f.write("Case 1: Pass\n")
-else:
-    f.write("Case 1: Fail\n")
-
-# Case 2
-mergedTracks = pd.merge(tracksInput, tracksOutput, on='TrackId', suffixes=('_input', '_output'))
-if (mergedTracks['UnitPrice_output'] - mergedTracks['UnitPrice_input'] < 1).all():
-    f.write("Case 2: Pass\n")
-else:
-    f.write("Case 2: Fail\n")
-
-# close test result file
-f.close()
+# Transform
+tracks["UnitPrice"] = tracks["UnitPrice"].apply(lambda x: math.ceil(x)).apply(lambda x: x+1)
+                             
+# Load
+tracks.to_csv(outputPath, index=False)
