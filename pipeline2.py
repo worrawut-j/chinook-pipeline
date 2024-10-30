8f279cbbfd59a02520b7e9c8c61f0d7a592f1dff
worrawut-j <152259934+worrawut-j@users.noreply.github.com>
2024-10-30 15:11:22 +0700

    Create pipeline2.py

---
+++ pipeline2.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import math
+import os
+import configparser
+
+# Get the parent path of the notebook
+notebook_path = dbutils.entry_point.getDbutils().notebook().getContext().notebookPath().get()
+parent_path = os.path.dirname('/Workspace' + notebook_path)
+os.chdir(parent_path)
+
+# read config file
+config = configparser.ConfigParser()
+config.read('./pipeline.conf')
+inputPath = config.get('DEFAULT', 'INPUT_PATH')
+outputPath = config.get('DEFAULT', 'OUTPUT_PATH_THB')
+
+# Extract
+tracks = pd.read_csv(inputPath)
+
+# Transform
+tracks["UnitPrice"] = tracks["UnitPrice"].apply(lambda x: x*33.77)
+                             
+# Load
+tracks.to_csv(outputPath, index=False)
